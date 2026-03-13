bfc72a17d5ff1c1120e0674db67afa3aac003523
Gurv-Chahal <gurveer.chahal15@icloud.com>
2026-03-13 15:47:28 -0700

    fixing pipeline synth

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -9,7 +9,12 @@ export class CdkCicdStack extends cdk.Stack {
     new CodePipeline(this, 'AwesomePipeline', {
       pipelineName: 'AwesomePipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Gurv-Chahal')
+        input: CodePipelineSource.gitHub('Gurv-Chahal/cdk-cicd', 'main'),
+        commands: [
+            'npm ci',
+            'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk.out'
       })
     })
 
